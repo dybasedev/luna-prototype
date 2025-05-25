72d3dcd2f3dff03b182c5285f0fa2ab365f60585
Chongyi <xpz3847878@gmail.com>
2025-05-26 05:12:52 +0800

    增加工具类

---
+++ src/Showcase/UI.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Dybasedev\LunaPrototype\Showcase;
+
+use Dybasedev\LunaPrototype\Showcase\Structures\Column;
+use Dybasedev\LunaPrototype\Showcase\Structures\Field;
+
+/**
+ * UI 工具类，快速创建组件的工具类
+ */
+class UI
+{
+    /**
+     * 创建表单字段
+     *
+     * @param string|array $name
+     * @param string|null $key
+     * @return Field
+     */
+    public static function field(string|array $name, ?string $key = null): Field
+    {
+        return new Field($name, $key);
+    }
+
+    /**
+     * 创建数据表的列
+     *
+     * @param string|array $name
+     * @param string|null $key
+     * @return Column
+     */
+    public static function column(string|array $name, ?string $key = null): Column
+    {
+        return new Column($name, $key);
+    }
+}
